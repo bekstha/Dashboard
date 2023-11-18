bebd77ea3f645d66d071ebadd5e10a124555206b
Bibek <study.bibekshrestha@gmail.com>
2023-11-18 15:14:06 +0200

    updated layouts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,80 @@
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { useStateContext } from "../contexts/ContextProvider";
+import MenuIcon from "@mui/icons-material/Menu";
+import UserProfile from "./UserProfile";
+import { useEffect } from "react";
+
+const NavBtn = ({ title, customFunction, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={customFunction}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />{" "}
+      {icon}{" "}
+    </button>
+  </TooltipComponent>
+);
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const {
+    activeMenu,
+    setActiveMenu,
+    isClicked,
+    setIsClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext();
 
-export default Navbar
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    if(screenSize <= 900){
+      setActiveMenu(false)
+    }else{
+      setActiveMenu(true)
+    }
+  }, [screenSize])
+
+  return (
+    <div className="flex justify-between p-2 md:mx-6 relative">
+      <NavBtn
+        title="Menu"
+        customFunction={() =>
+          setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+        }
+        color="blue"
+        icon={<MenuIcon />}
+      />
+
+      <div className="flex">
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img src="../assets/react.svg" className="rounded-full h-8 w-8" />
+            <p>
+              <span>Admin</span>
+            </p>
+          </div>
+        </TooltipComponent>
+        {isClicked.userProfile && <UserProfile />}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
