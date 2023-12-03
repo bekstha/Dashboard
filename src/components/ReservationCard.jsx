5bb88d6213ed7626a3464cf121aff55bf62ce738
asmita143 <89544722+asmita143@users.noreply.github.com>
2023-12-04 01:35:12 +0200

    tOtal number of pax function

--- src/components/ReservationCard.jsx
+++ src/components/ReservationCard.jsx
@@ -1,9 +1,9 @@
 import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useState } from "react";
-import { Button, Modal } from "antd";
+import { Button, Modal, Statistic } from "antd";
 import { InputLabel, Input } from "./Input";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { EditOutlined, DeleteOutlined, UserOutlined } from "@ant-design/icons";
 import useReservation from "../hooks/useReservation";
 
 const TableHeader = ({ label }) => (
@@ -101,144 +101,159 @@ const ReservationCard = ({ reservations = [] }) => {
   const reservationById = reservations?.filter(
     (reservation) => reservation?.id === selectedID
   );
+  const totalGuestCount = reservations.reduce(
+    (total, reservation) => total + Number(reservation.guestCount),
+    0
+  );
 
   return (
-    <table className="table-fixed min-w-full bg-white border border-gray-300 lg:text-lg text-xs">
-      <thead>
-        <tr>
-          <TableHeader label="Time" />
-          <TableHeader label="Pax" />
-          <TableHeader label="Name" />
-          <TableHeader label="Email" />
-          <TableHeader label="Phone number" />
-          <TableHeader label="Status" />
-          <TableHeader label="Decision" />
-          <TableHeader label="Actions" />
-        </tr>
-      </thead>
-      <tbody>
-        {reservations?.map((item) => (
-          <tr key={item.id}>
-            <TableContent label={item.reservationTime} />
-            <TableContent label={item.guestCount} />
-            <TableContent label={`${item.firstname} ${item.lastname}`} />
-            <TableContent label={item.email} />
-            <TableContent label={item.phoneNumber} />
-            <TableContent label={item.status} />
-            <TableContent
-              label={
-                <div className="flex">
-                  <a
-                    href={`http://localhost:5174/reservation/${item.id}/decline`}
-                    className="bg-orange-300 hover:bg-orange-400 px-3 py-1 rounded-md text-xs"
-                  >
-                    Decline
-                  </a>
-                  <a
-                    href={`http://localhost:5174/reservation/${item.id}/approve`}
-                    className="ml-2 bg-red-300 hover:bg-red-400 px-3 py-1 rounded-md text-xs"
-                  >
-                    Approve
-                  </a>
-                </div>
-              }
-            />
-            <TableContent
-              label={
-                <div className="flex">
-                  <button
-                    className="bg-orange-300 hover:bg-orange-400 px-3 py-1 rounded-md text-xs"
-                    onClick={() => modifyReservation(item.id, item)}
-                  >
-                    <EditOutlined className="mr-1" />
-                  </button>
-                  <button
-                    className="ml-2 bg-red-300 hover:bg-red-400 px-3 py-1 rounded-md text-xs"
-                    onClick={() => removeReservation(item.id)}
-                  >
-                    <DeleteOutlined className="mr-1" />
-                  </button>
-                </div>
-              }
-            />
+    <div>
+      <div className="bg-gray-200 p-4 flex items-center w-full">
+        <Statistic
+          title="Total Guests"
+          value={totalGuestCount}
+          prefix={<UserOutlined />}
+        />
+      </div>
+      <table className="table-fixed min-w-full bg-white border border-gray-300 lg:text-lg text-xs">
+        <thead>
+          <tr>
+            <TableHeader label="Time" />
+            <TableHeader label="Pax" />
+            <TableHeader label="Name" />
+            <TableHeader label="Email" />
+            <TableHeader label="Phone number" />
+            <TableHeader label="Status" />
+            <TableHeader label="Decision" />
+            <TableHeader label="Actions" />
           </tr>
-        ))}
-        <Modal open={isOpen} onCancel={() => setIsOpen(false)}>
-          {selectedID}
-          <form onSubmit={handleReservationSubmit}>
-            <div className="flex items-center justify-between gap-4">
-              <div>
-                <InputLabel label="First name" />
+        </thead>
+        <tbody>
+          {reservations?.map((item) => (
+            <tr key={item.id}>
+              <TableContent label={item.reservationTime} />
+              <TableContent label={item.guestCount} />
+              <TableContent label={`${item.firstname} ${item.lastname}`} />
+              <TableContent label={item.email} />
+              <TableContent label={item.phoneNumber} />
+              <TableContent label={item.status} />
+              <TableContent
+                label={
+                  <div className="flex">
+                    <a
+                      href={`http://localhost:5173/reservation/${item.id}/decline`}
+                      className="bg-orange-300 hover:bg-orange-400 px-3 py-1 rounded-md text-xs"
+                    >
+                      Decline
+                    </a>
+                    <a
+                      href={`http://localhost:5173/reservation/${item.id}/approve`}
+                      className="ml-2 bg-red-300 hover:bg-red-400 px-3 py-1 rounded-md text-xs"
+                    >
+                      Approve
+                    </a>
+                  </div>
+                }
+              />
+              <TableContent
+                label={
+                  <div className="flex">
+                    <button
+                      className="bg-orange-300 hover:bg-orange-400 px-3 py-1 rounded-md text-xs"
+                      onClick={() => modifyReservation(item.id, item)}
+                    >
+                      <EditOutlined className="mr-1" />
+                    </button>
+                    <button
+                      className="ml-2 bg-red-300 hover:bg-red-400 px-3 py-1 rounded-md text-xs"
+                      onClick={() => removeReservation(item.id)}
+                    >
+                      <DeleteOutlined className="mr-1" />
+                    </button>
+                  </div>
+                }
+              />
+            </tr>
+          ))}
+          <Modal open={isOpen} onCancel={() => setIsOpen(false)}>
+            {selectedID}
+            <form onSubmit={handleReservationSubmit}>
+              <div className="flex items-center justify-between gap-4">
+                <div>
+                  <InputLabel label="First name" />
+                  <Input
+                    placeholder="John"
+                    name="firstname"
+                    value={firstname || reservationById[0]?.firstname}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div>
+                  <InputLabel label="Last name" />
+                  <Input
+                    placeholder="Doe"
+                    name="lastname"
+                    value={lastname || reservationById[0]?.lastname}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className="mt-2">
+                <InputLabel label="Email" />
                 <Input
-                  placeholder="John"
-                  name="firstname"
-                  value={firstname || reservationById[0]?.firstname}
+                  type="email"
+                  placeholder="example@mail.com"
+                  name="email"
+                  value={email || reservationById[0]?.email}
                   onChange={handleInputChange}
                 />
               </div>
-              <div>
-                <InputLabel label="Last name" />
+              <div className="mt-2">
+                <InputLabel label="Phone number" />
                 <Input
-                  placeholder="Doe"
-                  name="lastname"
-                  value={lastname || reservationById[0]?.lastname}
+                  type="tel"
+                  placeholder="+358411103121"
+                  name="phoneNumber"
+                  value={phoneNumber || reservationById[0]?.phoneNumber}
                   onChange={handleInputChange}
                 />
               </div>
-            </div>
-            <div className="mt-2">
-              <InputLabel label="Email" />
-              <Input
-                type="email"
-                placeholder="example@mail.com"
-                name="email"
-                value={email || reservationById[0]?.email}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mt-2">
-              <InputLabel label="Phone number" />
-              <Input
-                type="tel"
-                placeholder="+358411103121"
-                name="phoneNumber"
-                value={phoneNumber || reservationById[0]?.phoneNumber}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mt-2">
-              <InputLabel label="Number of guest" />
-              <Input
-                type="number"
-                placeholder="1"
-                min="1"
-                max="14"
-                name="guestCount"
-                value={guestCount || reservationById[0]?.guestCount}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mt-2 flex items-center justify-between gap-4">
-              <div className="flex-1">
-                <InputLabel label="Time" />
+              <div className="mt-2">
+                <InputLabel label="Number of guest" />
                 <Input
-                  type="time"
-                  name="reservationTime"
-                  value={reservationTime || reservationById[0]?.reservationTime}
+                  type="number"
+                  placeholder="1"
+                  min="1"
+                  max="14"
+                  name="guestCount"
+                  value={guestCount || reservationById[0]?.guestCount}
                   onChange={handleInputChange}
                 />
               </div>
-            </div>
-            <Button
-              onClick={(event) => handleReservationSubmit(event)}
-              className="md:w-full w-full mt-8"
-            >
-              Submit Reservation
-            </Button>
-          </form>
-        </Modal>
-      </tbody>
-    </table>
+              <div className="mt-2 flex items-center justify-between gap-4">
+                <div className="flex-1">
+                  <InputLabel label="Time" />
+                  <Input
+                    type="time"
+                    name="reservationTime"
+                    value={
+                      reservationTime || reservationById[0]?.reservationTime
+                    }
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <Button
+                onClick={(event) => handleReservationSubmit(event)}
+                className="md:w-full w-full mt-8"
+              >
+                Submit Reservation
+              </Button>
+            </form>
+          </Modal>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
